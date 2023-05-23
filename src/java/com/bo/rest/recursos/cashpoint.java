b7fdc181e9f5be4d3edf543cf75bf7666e1289c4
Alexandro Arauco Sanchez <alexandro.arauco@incluit.com>
2023-05-22 22:25:36 -0400

    Cashpoint - new endpoints

--- src/java/com/bo/rest/recursos/cashpoint.java
+++ src/java/com/bo/rest/recursos/cashpoint.java
@@ -3,9 +3,9 @@ package com.bo.rest.recursos;
 import com.bo.rest.controlador.Login;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -14,21 +14,47 @@ import javax.ws.rs.core.Response;
  *
  * @author IGNACIO
  */
-@Path("/{source}")
+@Path("/cashpoint")
 public class cashpoint {
 
     @POST
     @Path("/token")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getToken(@PathParam("source") String source, String body) {
-        if (source.equals("partner") || source.equals("cashpoint")) {
-            Login login = new Login();
-            return Response.ok().entity(login.singIn(source, body)).build();
-        } else {
-            return Response.serverError().build();
-        }
+    public Response getToken(String body) {
+        Login login = new Login();
+        return Response.ok().entity(login.singIn("cashpoint", body)).build();
 
+    }
+
+    @GET
+    @Path("/transactions/pending")
+    public Response getPendingTransactions() {
+        return Response.ok().entity("").build();
+    }
+
+    @POST
+    @Path("/transactiondetails")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getDetailTransactions() {
+        return Response.ok().entity("").build();
+    }
+
+    @POST
+    @Path("/request/validate-transaction")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response validateTransaction() {
+        return Response.ok().entity("").build();
+    }
+
+    @POST
+    @Path("/gave")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response gave() {
+        return Response.ok().entity("").build();
     }
 
 }
